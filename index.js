5e4e1ee8d195c795ed6746352fe22e8716bfddb2
Alex Bolyakov <alex.bolyakov.crs@me.com>
2024-02-27 14:34:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+const { getHouses } = require('controller.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/api/houses', getHouses);
+
+app.get('/', (req, res) => {
+     res.sendFile('static/index.html', {root: __dirname});
+ })
+
+app.listen(4000, () => console.log(`Server running on port 4000`))
